c24654f4a577f8986d9caa0609fd72094ab86d61
phw11 <hwpark11@kakao.com>
2021-09-03 17:07:51 +0900

    feat : 그룹과 집합 예제 추가

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -17,6 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @SpringBootTest
 @Transactional
@@ -181,5 +184,57 @@ class QuerydslBasicTest {
     assertThat(fetchResults.getLimit()).isEqualTo(2);
     assertThat(fetchResults.getOffset()).isEqualTo(1);
     assertThat(fetchResults.getResults().size()).isEqualTo(2);
+  }
+
+  @Test
+  void testAggregation() throws Exception {
+    // given
+
+    // when
+    List<Tuple> result =
+        queryFactory
+            .select(
+                member.count(),
+                member.age.sum(),
+                member.age.avg(),
+                member.age.max(),
+                member.age.min())
+            .from(member)
+            .fetch();
+
+    // then
+    Tuple tuple = result.get(0);
+
+    assertThat(tuple.get(member.count())).isEqualTo(4);
+    assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+    assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+    assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    assertThat(tuple.get(member.age.max())).isEqualTo(40);
+  }
+
+  @Test
+  void testGroupBy() throws Exception {
+    // given
+
+    // when
+    // 팀의 이름과 각 팀의 평균 연령을 구하라
+    List<Tuple> result =
+        queryFactory
+            .select(team.name, member.age.avg())
+            .from(member)
+            .join(member.team, team)
+            .groupBy(team.name)
+            //                .having()
+            .fetch();
+
+    Tuple teamA = result.get(0);
+    Tuple teamB = result.get(1);
+
+    // then
+    assertThat(teamA.get(team.name)).isEqualTo("teamA");
+    assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+    assertThat(teamB.get(team.name)).isEqualTo("teamB");
+    assertThat(teamB.get(member.age.avg())).isEqualTo(35);
   }
 }
