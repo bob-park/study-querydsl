73f11f97ff12df6bd5bd06dcf8c72d2fe4839196
phw11 <hwpark11@kakao.com>
2021-09-03 18:06:29 +0900

    feat : 상수, 문자 더하기 예제 추가

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -523,6 +524,43 @@ class QuerydslBasicTest {
             .fetch();
 
     // then
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
+  }
+
+  @Test
+  void testConstant() throws Exception {
+    // given
+
+    // when
+    List<Tuple> result =
+        queryFactory.select(member.username, Expressions.constant("A")).from(member).fetch();
+
+    // then
+
+    for (Tuple tuple : result) {
+      System.out.println("tuple = " + tuple);
+    }
+  }
+
+  @Test
+  void testConcat() throws Exception {
+    // given
+
+    // when
+    // {username}_{age}
+    List<String> result =
+        queryFactory
+            .select(
+                member
+                    .username
+                    .concat("_")
+                    .concat(member.age.stringValue())) // stringValue() 자주 쓰인다. ENUM 처리시 아주 유용함
+            .from(member)
+            .where(member.username.eq("member1"))
+            .fetch();
+
     for (String s : result) {
       System.out.println("s = " + s);
     }
