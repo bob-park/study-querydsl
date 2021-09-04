0ac6cd8d309684c185ca48b7960d30805e999b6f
phw11 <hwpark11@kakao.com>
2021-09-04 15:03:18 +0900

    feat : Custom repository + QueryDSL 사용 예제 추가

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -22,86 +22,61 @@ import static org.junit.jupiter.api.Assertions.*;
 @Rollback(false)
 class MemberRepositoryTest {
 
-    @Autowired
-    private EntityManager em;
+  @Autowired private EntityManager em;
 
-    @Autowired
-    private MemberRepository memberRepository;
+  @Autowired private MemberRepository memberRepository;
 
-    @Test
-    void testBasic() throws Exception {
-        // given
-        Member member1 = new Member("member1", 10);
+  @Test
+  void testBasic() throws Exception {
+    // given
+    Member member1 = new Member("member1", 10);
 
-        memberRepository.save(member1);
+    memberRepository.save(member1);
 
-        // when
-        Member findMember = memberRepository.findById(member1.getId()).get();
+    // when
+    Member findMember = memberRepository.findById(member1.getId()).get();
 
-        List<Member> result1 = memberRepository.findAll();
+    List<Member> result1 = memberRepository.findAll();
 
-        List<Member> result2 = memberRepository.findByUsername("member1");
+    List<Member> result2 = memberRepository.findByUsername("member1");
 
-        // then
-        assertThat(findMember).isEqualTo(member1);
-        assertThat(result1).containsExactly(member1);
-        assertThat(result2).containsExactly(member1);
-    }
+    // then
+    assertThat(findMember).isEqualTo(member1);
+    assertThat(result1).containsExactly(member1);
+    assertThat(result2).containsExactly(member1);
+  }
 
-//    @Test
-//    void testBasicQuerydsl() throws Exception {
-//        // given
-//        Member member1 = new Member("member1", 10);
-//
-//        memberRepository.save(member1);
-//
-//        // when
-//
-//        Member findMember = memberRepository.findById(member1.getId()).get();
-//
-//        List<Member> result1 = memberRepository.findAllQuerydsl();
-//
-//        List<Member> result2 = memberRepository.findByUsernameQuerydsl("member1");
-//
-//        // then
-//        assertThat(findMember).isEqualTo(member1);
-//        assertThat(result1).containsExactly(member1);
-//        assertThat(result2).containsExactly(member1);
-//    }
-//
-//    @Test
-//    void testSearch() throws Exception {
-//        // given
-//        Team teamA = new Team("teamA");
-//        Team teamB = new Team("teamB");
-//
-//        em.persist(teamA);
-//        em.persist(teamB);
-//
-//        Member member1 = new Member("member1", 10, teamA);
-//        Member member2 = new Member("member2", 20, teamA);
-//        Member member3 = new Member("member3", 30, teamB);
-//        Member member4 = new Member("member4", 40, teamB);
-//
-//        em.persist(member1);
-//        em.persist(member2);
-//        em.persist(member3);
-//        em.persist(member4);
-//
-//        // when
-//        MemberSearchCondition condition = new MemberSearchCondition();
-//
-//        condition.setAgeGoe(35);
-//        condition.setAgeLoe(45);
-//        condition.setTeamName("teamB");
-//
-//        //    List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
-//        List<MemberTeamDto> result = memberJpaRepository.search(condition);
-//
-//        // then
-//
-//        assertThat(result).extracting("username").containsExactly("member4");
-//    }
+  @Test
+  void testSearch() throws Exception {
+    // given
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
 
+    em.persist(teamA);
+    em.persist(teamB);
 
+    Member member1 = new Member("member1", 10, teamA);
+    Member member2 = new Member("member2", 20, teamA);
+    Member member3 = new Member("member3", 30, teamB);
+    Member member4 = new Member("member4", 40, teamB);
+
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
+
+    // when
+    MemberSearchCondition condition = new MemberSearchCondition();
+
+    condition.setAgeGoe(35);
+    condition.setAgeLoe(45);
+    condition.setTeamName("teamB");
+
+    //    List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+    List<MemberTeamDto> result = memberRepository.search(condition);
+
+    // then
+
+    assertThat(result).extracting("username").containsExactly("member4");
+  }
 }
